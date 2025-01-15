f294720d7d43e862ff76cbcdd5096e987c232067
François Godin <francois.godin02@etud.univ-paris8.fr>
2025-01-15 21:21:17 +0100

    🚚 init 1.2 project from 1.1

---
+++ basic_modelisation-1.2/main.c
@@ -0,0 +1,87 @@
+#include <GL4D/gl4duw_SDL2.h>
+#include <GL4D/gl4dg.h>
+#include <stdio.h>
+#include <math.h>
+
+static void init(void);
+/* TODO : gérer le retaillage de la fenêtre */
+/* static void resize(int width, int height); */
+static void draw(void);
+static void sortie(void);
+
+static GLuint _wW = 640, _wH = 480;
+static GLuint _cubeId = 0;
+static GLuint _pId = 0;
+
+int main(int argc, char ** argv) {
+  if(!gl4duwCreateWindow(argc, argv, "Ateliers API8 - modélisation", GL4DW_POS_CENTERED, GL4DW_POS_CENTERED,
+			 _wW, _wH, GL4DW_OPENGL | GL4DW_RESIZABLE | GL4DW_SHOWN)) {
+    fprintf(stderr, "Erreur lors de la création de la fenêtre\n");
+    return 1;
+  }
+  init();
+  atexit(sortie);
+  gl4duwDisplayFunc(draw);
+  gl4duwMainLoop();
+  return 0;
+}
+
+void init(void) {
+  glClearColor(0.5f, 0.0f, 0.0f, 1.0f);
+  _cubeId = gl4dgGenCubef();
+  // Créer un programme shader à partir de hello.vs et hello.fs, qui pourra s'occuper du rendu.
+  _pId = gl4duCreateProgram("<vs>shaders/hello.vs", "<fs>shaders/hello.fs", NULL);
+  gl4duGenMatrix(GL_FLOAT, "modelView");
+
+  // Appliquer une matrice de projection, elle sera envoyé lors de l'appel à sendMatrices()
+  gl4duGenMatrix(GL_FLOAT, "projection");
+  gl4duBindMatrix("projection");
+  gl4duLoadIdentityf();
+  
+  GLfloat bottom = (-1.0f * _wH) / _wW;
+  GLfloat top    = (1.0f * _wH) / _wW;
+  gl4duFrustumf(-1.0f, 1.0f, bottom, top, 1.0f, 100.0f);
+
+  // Afficher le cube en file de fer
+  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+}
+
+// Renvoyer le delta temps écoulé entre deux appels
+static double get_dt(void) {
+  static double t0 = 0.0f;
+  // Récupérer le temps écoulé en ms entre le démarrage du programme et la frame en cours
+  double t = gl4dGetElapsedTime();
+  // Calculer le temps écoulé depuis la frame précédente en seconde
+  double dt = (t - t0) / 1000.0f;
+  t0 = t;
+  return dt;
+}
+
+void draw(void) {
+  static GLfloat rot = 0.0f;
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glUseProgram(_pId);
+
+  gl4duBindMatrix("modelView");
+  gl4duLoadIdentityf(); // matrice neutre utilisée pour l'initialisation 
+  
+  // -2 sur les z car notre frustum a un near à 1 donc on commence à voir non plus à 0 mais -1 (éloignement)
+  // on éloigne à -3 quand on passe à un cube car il a une profondeur (+1 en avant -1 en arrière)
+  gl4duTranslatef(sin(rot), 0.0f, -3.0f);
+  // Convertir rot de radian à degré
+  GLfloat angle = 180.0f * rot / M_PI;
+  // Rotation autour de l'axe des y 
+  gl4duRotatef(angle, 0.0f, 1.0f, 0.0f); 
+  
+  // Les modifications de matrices doivent être envoyées au GPU avant de dessinner
+  gl4duSendMatrices();
+
+  gl4dgDraw(_cubeId);
+  glUseProgram(0);
+
+  rot += 2.0f * M_PI * get_dt();
+}
+
+void sortie(void) {
+  gl4duClean(GL4DU_ALL);
+}
