801bab07a798502a069de8082e4efcbdd241eeca
François Godin <francois.godin02@etud.univ-paris8.fr>
2025-02-10 00:56:15 +0100

    create spectre.c from basic_modelisation-1.2 main.c  file

---
+++ demo_rgb-1.2/spectre.c
@@ -0,0 +1,92 @@
+#include <GL4D/gl4duw_SDL2.h>
+#include <GL4D/gl4dg.h>
+#include <stdio.h>
+#include <math.h>
+
+#define NB_E 16
+
+static void init(void);
+/* TODO : gérer le retaillage de la fenêtre */
+/* static void resize(int width, int height); */
+static void draw(void);
+static void sortie(void);
+
+static GLuint _wW = 640, _wH = 480;
+static GLuint _cubeId = 0;
+static GLuint _pId = 0;
+
+static int _hauteurs[NB_E];
+
+int main(int argc, char ** argv) {
+  if(!gl4duwCreateWindow(argc, argv, "Ateliers API8 - modélisation", GL4DW_POS_CENTERED, GL4DW_POS_CENTERED,
+			 _wW, _wH, GL4DW_OPENGL | GL4DW_RESIZABLE | GL4DW_SHOWN)) {
+    fprintf(stderr, "Erreur lors de la création de la fenêtre\n");
+    return 1;
+  }
+  init();
+  atexit(sortie);
+  gl4duwDisplayFunc(draw);
+  gl4duwMainLoop();
+  return 0;
+}
+
+void init(void) {
+  int i;
+  // Initialisation du tableau _hauteurs
+  for (i = 0; i < NB_E; ++i) {
+    // valeur aléatoire entre -128 et 127
+    _hauteurs[i] = -128 + (rand() & 0xFF);
+  }
+  glClearColor(0.5f, 0.0f, 0.0f, 1.0f);
+  _cubeId = gl4dgGenCubef();
+  // Créer un programme shader à partir de hello.vs et hello.fs, qui pourra s'occuper du rendu.
+  _pId = gl4duCreateProgram("<vs>shaders/hello.vs", "<fs>shaders/hello.fs", NULL);
+  gl4duGenMatrix(GL_FLOAT, "modelView");
+
+  // Appliquer une matrice de projection, elle sera envoyé lors de l'appel à sendMatrices()
+  gl4duGenMatrix(GL_FLOAT, "projection");
+  gl4duBindMatrix("projection");
+  gl4duLoadIdentityf();
+  
+  GLfloat bottom = (-1.0f * _wH) / _wW;
+  GLfloat top    = (1.0f * _wH) / _wW;
+  gl4duFrustumf(-1.0f, 1.0f, bottom, top, 1.0f, 100.0f);
+}
+
+void draw(void) {
+  int i, j;
+
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glUseProgram(_pId);
+
+  gl4duBindMatrix("modelView");
+  gl4duLoadIdentityf(); // matrice neutre utilisée pour l'initialisation 
+  
+  gl4duTranslatef(0.0, 0.0f, -1.1f);
+  for (i = 0; i < NB_E; ++i) {
+    // Ramener i dans la plage [-1, 1] 
+    GLfloat x = 2.0f * (i / (NB_E - 1.0f)) - 1.0f;
+    gl4duPushMatrix();
+    gl4duTranslatef(x, 0.0f, 0.0f);
+    for (j = 0; j <= abs(_hauteurs[i]); ++j) {
+      GLfloat y  = (_hauteurs[i] < 0 ? -j : j) / 256.0f;
+      gl4duPushMatrix();
+      gl4duTranslatef(0.0f, y, 0.0f);
+      // placer le scale en bout de chaine pour éviter qu'il n'agissse sur les translations précédentes
+      gl4duScalef(0.02f, 0.02f, 0.02f);
+      gl4duSendMatrices();
+      gl4duPopMatrix();
+      gl4dgDraw(_cubeId);
+    }
+    gl4duPopMatrix();
+  }
+
+  // Les modifications de matrices doivent être envoyées au GPU avant de dessinner
+  gl4duSendMatrices();
+
+  glUseProgram(0);
+}
+
+void sortie(void) {
+  gl4duClean(GL4DU_ALL);
+}
