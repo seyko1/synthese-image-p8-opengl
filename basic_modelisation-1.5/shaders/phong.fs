a6f18c1fdbbd250b24d2bb9986eeacb8b84fcfca
François Godin <francois.godin02@etud.univ-paris8.fr>
2025-03-24 20:58:29 +0100

    split operations and rename variables

--- basic_modelisation-1.5/shaders/phong.fs
+++ basic_modelisation-1.5/shaders/phong.fs
@@ -10,26 +10,33 @@ uniform vec4 couleur;
 uniform mat4 view;
 
 void main() {
-     vec3 Ld = normalize(modPos.xyz - lumpos.xyz);
-     
-     float il = 2.0 * clamp(dot(modNormal, -Ld), 0.0, 1.0);
+     /* Etape 1 - Calcul et application de l'intensité de la lumière (lumière diffuse) */
 
-     // L'oeil est toujours orienté dans cette direction en phase final (view model) ...
+     vec3 directionalLightVector = normalize(modPos.xyz - lumpos.xyz);
+     
+     float lightIntensity = 2.0 * clamp(dot(modNormal, -directionalLightVector), 0.0, 1.0);
+
+     fragColor = lightIntensity * couleur;
+
+     /* Etape 2 - Calcul et application du reflet de la lumière (lumière spéculaire) */
+
+     // Déduire le vecteur reflet
+     vec3 reflectVector = reflect(directionalLightVector, modNormal);
+
+     // formule magique pour extraire et appliquer les rotations stockées dans view au vecteur de reflet
+     reflectVector = normalize((transpose(inverse(view)) * vec4(reflectVector, 0.0)).xyz);
+
+     // L'oeil est toujours orienté dans cette direction en phase finale (view model) ...
      const vec3 viewVector = vec3(0.0, 0.0, -1.0);
 
-     // déduire le vecteur reflet
-     vec3 reflect = reflect(Ld, modNormal);
-     // formule magique pour extraire et appliquer les rotations stockées dans view aux vecteurs de reflet
-     reflect = normalize((transpose(inverse(view)) * vec4(reflect, 0.0)).xyz);
-     
      // L'intensité de spéculaire est un produit scalaire entre le vecteur reflet et le vecteur vue
      // (on en met un des deux en négatif pour comparer leur colinéarité dans la même direction)
-     float specularIntensity = clamp(dot(-reflect, viewVector), 0.0, 1.0);
+     float specularIntensity = clamp(dot(-reflectVector, viewVector), 0.0, 1.0);
      
-     // On ajoute une exponentielle (à 10 par exemple) pour éviter d'avoir une valeur trop progressive entre 0 et 1.
-     // On veut une valeur qui montre en flêche lorsqu'elle est proche de 1
+     // On ajoute une exponentielle élevée (à 10 par exemple) pour éviter d'avoir une valeur trop progressive entre 0 et 1.
+     // On veut une valeur qui montre en flêche lorsqu'elle est proche de 1.
      specularIntensity = pow(specularIntensity, 10.0);
 
      // La lumière spéculaire (blanche) vient s'ajouter (avec une intensité variable) à la lumière diffuse
-     fragColor = specularIntensity * vec4(1.0) + il * couleur;
+     fragColor += specularIntensity * vec4(1.0);
 }
