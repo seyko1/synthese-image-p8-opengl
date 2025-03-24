f07c945b816c811cea1a7ca8301ca2b37d004ffd
François Godin <francois.godin02@etud.univ-paris8.fr>
2025-03-24 19:31:36 +0100

    ✨ add reflect light for each fragment

--- basic_modelisation-1.5/shaders/phong.fs
+++ basic_modelisation-1.5/shaders/phong.fs
@@ -12,5 +12,20 @@ void main() {
      
      float il = 2.0 * clamp(dot(modNormal, -Ld), 0.0, 1.0);
 
-     fragColor = il * couleur;
+     // on reprend le vecteur en dur à partir ce ce qui a été défini dans le lookAt...
+     vec3 viewVector = normalize(vec3(0.0, -2.0, -4.0));
+
+     // vecteur reflet
+     vec3 reflect = reflect(Ld, modNormal);
+     
+     // L'intensité de spéculaire est un produit scalaire entre le vecteur reflet et le vecteur vue
+     // (on en met un des deux en négatif pour comparer leur colinéarité dans la même direction)
+     float specularIntensity = clamp(dot(-reflect, viewVector), 0.0, 1.0);
+     
+     // On ajoute une exponentielle (à 10 par exemple) pour éviter d'avoir une valeur trop progressive entre 0 et 1.
+     // On veut une valeur qui montre en flêche lorsqu'elle est proche de 1
+     specularIntensity = pow(specularIntensity, 10.0);
+
+     // La lumière spéculaire (blanche) vient s'ajouter (avec une intensité variable) à la lumière diffuse
+     fragColor = specularIntensity * vec4(1.0) + il * couleur;
 }
