cd6ea4170cc8ec919c866f14782d5af7efaeb414
François Godin <francois.godin02@etud.univ-paris8.fr>
2025-04-18 01:08:10 +0200

    ✨ Generate both texture and frame buffer, then bind them

--- fbo_convolution-1.0/main.c
+++ fbo_convolution-1.0/main.c
@@ -10,8 +10,10 @@ static void draw(void);
 static void sortie(void);
 
 static GLuint _wW = 640, _wH = 480;
-static GLuint _quadId = 0;
+static GLuint _sphereId = 0;
 static GLuint _pId = 0;
+static GLuint _fbo = 0;
+static GLuint _tex = 0;
 
 int main(int argc, char ** argv) {
   if(!gl4duwCreateWindow(argc, argv, "Ateliers API8 - Convolution FBO", GL4DW_POS_CENTERED, GL4DW_POS_CENTERED,
@@ -29,19 +31,59 @@ int main(int argc, char ** argv) {
 void init(void) {
   glClearColor(0.5f, 0.0f, 0.0f, 1.0f);
 
-  _quadId = gl4dgGenQuadf();
+  _sphereId = gl4dgGenSpheref(7, 7);
   _pId = gl4duCreateProgram("<vs>shaders/conv.vs", "<fs>shaders/conv.fs", NULL);
+
+  /* Gestion de la texture pour le fbo */
+
+  glGenTextures(1, &_tex),
+
+  // Bind texture
+  glBindTexture(GL_TEXTURE_2D, _tex);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, _wW, _wH, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+
+  // Debind texture
+  glBindTexture(GL_TEXTURE_2D, 0);
+
+  /* Gestion du FBO */
+  
+  glGenFramebuffers(1, &_fbo);
+
+  // Bind fbo
+  glBindFramebuffer(GL_FRAMEBUFFER, _fbo);
+  // Attacher la texture au fbo
+  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, _tex, 0);
+  // Debind fbo
+  glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
 
 void draw(void) {
+  // bind fbo
+  glBindFramebuffer(GL_FRAMEBUFFER, _fbo);
+
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glUseProgram(_pId);
   glUniform1f(glGetUniformLocation(_pId, "weight"), 1.1);
-  gl4dgDraw(_quadId);
+  gl4dgDraw(_sphereId);
+  // debind fbo -> rebind l ecran
+  glBindFramebuffer(GL_FRAMEBUFFER, 0);
   glUseProgram(0);
-
 }
 
 void sortie(void) {
+  if (_fbo) {
+    glDeleteFramebuffers(1, &_fbo);
+    _fbo = 0;
+  }
+  if (_tex) {
+    glDeleteTextures(1, &_tex);
+    _tex = 0;
+  }
   gl4duClean(GL4DU_ALL);
 }
