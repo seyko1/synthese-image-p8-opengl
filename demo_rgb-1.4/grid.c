c1234e9bd6839677489c7c57f0eeb0522792a76f
François Godin <francois.godin02@etud.univ-paris8.fr>
2025-04-27 21:03:48 +0200

    🎉 start demo_rgb-1.4 (raise grid vertices by audio)

---
+++ demo_rgb-1.4/grid.c
@@ -0,0 +1,99 @@
+#include <GL4D/gl4duw_SDL2.h>
+#include <GL4D/gl4df.h>
+#include <GL4D/gl4dh.h>
+#include <SDL_image.h>
+#include <GL4D/gl4dg.h>
+#include <stdio.h>
+#include <math.h>
+#include "audioHelper.h"
+
+#define NBE 1024
+
+static void init(void);
+static void draw(void);
+static void sortie(void);
+
+static GLuint _wW = 1280, _wH = 1024;
+static GLuint _gridId = 0;
+static GLuint _pId = 0;
+static GLuint _tex = 0;
+
+static GLint _hauteurs[NBE] = { 0 };
+static GLbyte _gridW = 32, _gridH = 32;
+
+void grid(int state) {
+  switch(state) {
+    case GL4DH_INIT:
+      init();
+      return;
+    case GL4DH_FREE:
+      sortie();
+      return;
+    case GL4DH_UPDATE_WITH_AUDIO:
+      int length = ahGetAudioStreamLength();
+      short * stream = (short*) ahGetAudioStream();
+      
+      /* Avancer de 2 pour ne prendre qu'une des 2 valeurs du stéréo */
+      for (int i = 0; i < length / 4; ++i) {
+        // 1024 valeurs d'oreille gauche
+        _hauteurs[i] = stream[2 * i];
+      }
+      return;
+    default:
+      draw();
+      return;
+  }
+}
+
+void init(void) {
+  _pId = gl4duCreateProgram("<vs>shaders/grid.vs", "<fs>shaders/grid.fs", NULL);
+  _gridId = gl4dgGenGrid2df(_gridW, _gridH);
+
+  glGenTextures(1, &_tex),
+  glBindTexture(GL_TEXTURE_2D, _tex);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glBindTexture(GL_TEXTURE_2D, 0);
+
+  gl4duGenMatrix(GL_FLOAT, "modelView");
+  gl4duGenMatrix(GL_FLOAT, "projection");
+  gl4duBindMatrix("projection");
+  gl4duLoadIdentityf();
+ 
+  gl4duFrustumf(-0.1f, 0.1f, (-0.1f * _wH) / _wW, (0.1f * _wH) / _wW, 0.1f, 1000.0f); 
+}
+
+void draw(void) {
+  glClearColor(0.5f, 0.0f, 0.0f, 1.0f);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+  
+  glUseProgram(_pId);
+  
+  glActiveTexture(GL_TEXTURE0);
+  glBindTexture(GL_TEXTURE_2D, _tex);
+  // Appliquer les valeurs de pixels à la texture
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_R16, _gridW, _gridH, 0, GL_RED, GL_UNSIGNED_SHORT, _hauteurs);
+  glUniform1i(glGetUniformLocation(_pId, "tex"), 0);
+  
+  gl4duBindMatrix("modelView");
+  gl4duLoadIdentityf();
+  gl4duLookAtf(-1.0f, 0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+  glTranslatef(0.0, 0.0, -1.0f);
+  gl4duSendMatrices();
+  
+  gl4dgDraw(_gridId);
+  
+  glBindTexture(GL_TEXTURE_2D, 0);
+  glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+  glUseProgram(0);
+  SDL_Delay(100);
+}
+
+void sortie(void) {
+  if (_tex) {
+    glDeleteTextures(1, &_tex);
+    _tex = 0;
+  }
+}
---
+++ demo_rgb-1.4/shaders/grid.vs
@@ -0,0 +1,13 @@
+#version 330
+
+layout(location = 0) in vec3 pos;
+layout(location = 1) in vec3 normal;
+layout(location = 2) in vec2 texCoord;
+
+uniform mat4 projection, modelView;
+uniform sampler2D tex;
+
+void main() {
+    vec3 p = pos + vec3(0.0, texture(tex, texCoord).r / 4.0 , 0.0);
+    gl_Position = projection * modelView * vec4(p, 1.0);
+}
