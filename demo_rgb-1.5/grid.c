7fc6c58e15cf633cb4a8a7b43cf6f60f47c9a974
François Godin <francois.godin02@etud.univ-paris8.fr>
2025-05-11 18:55:46 +0200

    🎉 start demo_rgb-1.5  from demo_rgb-1.4

---
+++ demo_rgb-1.5/grid.c
@@ -0,0 +1,109 @@
+#include <GL4D/gl4duw_SDL2.h>
+#include <GL4D/gl4df.h>
+#include <GL4D/gl4dh.h>
+#include <SDL_image.h>
+#include <GL4D/gl4dg.h>
+#include <stdio.h>
+#include <math.h>
+#include "audioHelper.h"
+
+#define HEIGHTS_SIZE 16
+
+static void init(void);
+static void draw(void);
+static void sortie(void);
+
+static GLuint _wW = 1280, _wH = 1024;
+static GLuint _gridId = 0;
+static GLuint _pId = 0;
+static GLuint _tex = 0;
+
+static GLuint _hauteurs[HEIGHTS_SIZE] = { 0 };
+static GLbyte _gridW = 64, _gridH = 64;
+
+/* Remplace le 1er élément du tableau et décale les autres */
+void shift(short v) {
+  for (int i = HEIGHTS_SIZE - 1; i > 0; i--) {
+    _hauteurs[i] = _hauteurs[i - 1];
+  }
+
+  _hauteurs[0] = v;
+}
+
+static short moyenne = 0;
+void grid(int state) {
+  switch(state) {
+    case GL4DH_INIT:
+      init();
+      return;
+    case GL4DH_FREE:
+      sortie();
+      return;
+    case GL4DH_UPDATE_WITH_AUDIO:
+      int length = ahGetAudioStreamLength();
+      short * stream = (short*) ahGetAudioStream(); /* échantillon de 4096 valeurs */
+      int value = 0;
+      /* Avancer de 2 pour ne prendre qu'une des 2 valeurs du stéréo */
+      for (int i = 0; i < length / 4; ++i) {
+        // somme des 1024 valeurs d'oreille gauche
+        value += fabs(stream[2 * i]);
+      }
+      moyenne = value / (length / 4);
+      shift(moyenne);
+      return;
+    default:
+      draw();
+      return;
+  }
+}
+
+void init(void) {
+  _pId = gl4duCreateProgram("<vs>shaders/grid.vs", "<fs>shaders/grid.fs", NULL);
+  _gridId = gl4dgGenGrid2df(_gridW, _gridH);
+
+  glGenTextures(1, &_tex),
+  glBindTexture(GL_TEXTURE_2D, _tex);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glBindTexture(GL_TEXTURE_2D, 0);
+
+  gl4duGenMatrix(GL_FLOAT, "modelView");
+  gl4duGenMatrix(GL_FLOAT, "projection");
+  gl4duBindMatrix("projection");
+  gl4duLoadIdentityf();
+ 
+  gl4duFrustumf(-0.1f, 0.1f, (-0.1f * _wH) / _wW, (0.1f * _wH) / _wW, 0.1f, 1000.0f); 
+}
+
+void draw(void) {
+  GLfloat pas[] = { 1.0f / (_gridW - 1.0f), 1.0f / (_gridH - 1.0f) };
+
+  glClearColor(0.0f, 0.7f, 0.7f, 1.0f);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  glUseProgram(_pId);
+  
+  glActiveTexture(GL_TEXTURE0);
+  glBindTexture(GL_TEXTURE_2D, _tex);
+  // Envoyer les hauteurs dans une texture de dimension 1 * HEIGHTS_SIZE
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_R16, 1, HEIGHTS_SIZE, 0, GL_RED, GL_SHORT, _hauteurs);
+  glUniform1i(glGetUniformLocation(_pId, "tex"), 0);
+  glUniform2fv(glGetUniformLocation(_pId, "pas"), 1, pas);
+  
+  gl4duBindMatrix("modelView");
+  gl4duLoadIdentityf();
+  gl4duLookAtf(-1.0f, 0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+  gl4duSendMatrices();
+  
+  gl4dgDraw(_gridId);
+  
+  glBindTexture(GL_TEXTURE_2D, 0);
+  glUseProgram(0);
+}
+
+void sortie(void) {
+  if (_tex) {
+    glDeleteTextures(1, &_tex);
+    _tex = 0;
+  }
+}
---
+++ demo_rgb-1.5/shaders/grid.vs
@@ -0,0 +1,88 @@
+#version 330
+
+layout(location = 0) in vec3 pos;
+layout(location = 1) in vec3 normal;
+layout(location = 2) in vec2 texCoord;
+
+uniform mat4 projection, modelView;
+uniform sampler2D tex;
+uniform vec2 pas;
+
+out float lightIntensity;
+
+/* Tableau de décalage pour parcourir les 6 points voisins du vertex */
+vec2 offset[7] = vec2[7](
+    vec2(pas.x, 0.0),
+    vec2(pas.x,  pas.y),
+    vec2(0.0,  pas.y),
+    vec2(-pas.x,  0.0),
+    vec2(-pas.x, -pas.y),
+    vec2(0.0, -pas.y),
+    vec2(0.0,  0.0)
+);
+
+/* Donne la hauteur d'une coordonnée selon l'intensité de rouge du texel correspondant */
+float hauteur(vec2 tc) {
+    /* distance de la coordonnée du centre de la grille */
+    float d = distance(vec2(0.5), tc);
+
+    /* Normaliser y à l'echelle [-0.5; 0.5] de la grille ? */
+    float y = d / (1.42 / 2.0);
+    return texture(tex, vec2(1.0, y)).r / 4.0;
+}
+
+/* Calculer la normale d'un des triangles reliés au vertex.
+ * On a besoin de deux de ses côtés en entrée pour calculer sa normale.
+ */ 
+vec3 tnormale(vec3 a, vec3 b) {
+    return normalize(cross(a, b));
+}
+
+/* Position d'un sommet voisin = position initiale + offset, et modification de sa hauteur selon son intensité de rouge */
+vec3 position(vec3 pi, vec2 tc, vec2 offset) {
+    return pi + vec3(
+        offset.x * 2.0,
+        hauteur(tc),
+        -(offset.y * 2.0)
+    );
+}
+
+/* Calculer la normale du vertex, en moyennant les normales des triangles qui sont autour. */
+vec3 normale(vec3 pi, vec2 tc) {
+    // normale du sommet
+    vec3 n = vec3(0);
+
+    // On a 7 points constituant les 6 triangles autour du sommet.
+    vec3 points[7];
+
+    /* On parcourt nos 7 coordonnées spatiales  */
+    for (int i = 0; i < 7; ++i) {
+        points[i] = position(pi, tc + offset[i], offset[i]);
+    }
+
+    /* On parcourt chaque points pour trouver les deux côtés de chaque triangles */
+    for (int i = 0; i < 6; ++i) {
+        /* Calculer 2 côtés du triangle lié au point i 
+         * Le point i est un voisin dans la texture
+         */
+        vec3 a = points[6];
+        vec3 b = points[i];
+        vec3 c = points[(i + 1) % 6];
+
+        n += tnormale(b - a, c - b);
+    }
+    return n /= 6.0;
+} 
+
+void main() {
+    vec3 Ld = normalize(vec3(0, -1, -1));
+
+
+    // modifier la hauteur du vertex en piochant une hauteur stockée dans tex.
+    vec3 p = pos + vec3(0.0, hauteur(texCoord), 0.0);
+
+    vec3 vsoNormal = normalize((transpose(inverse(modelView)) * vec4(normale(pos, texCoord), 1.0)).xyz);
+    lightIntensity = dot(vsoNormal, -Ld);
+
+    gl_Position = projection * modelView * vec4(p, 1.0);
+}
