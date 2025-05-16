c330b86fb5abc8cbeb686cd48b2b2e75b4edb0f3
François Godin <francois.godin02@etud.univ-paris8.fr>
2025-05-16 09:41:40 +0200

    some min shaders was broken..

--- fg-64ko/min-shaders/credits.fs
+++ fg-64ko/min-shaders/credits.fs
@@ -1,2 +1,9 @@
 #version 330
-uniform sampler2D tex;in vec2 vsoTexCoord;out vec4 fragColor;void main(void){vec4 b=texture(tex,vsoTexCoord);fragColor=vec4(b.rgb,1.);}
+uniform sampler2D tex;
+in  vec2 vsoTexCoord;
+out vec4 fragColor;
+
+void main(void) {
+  vec4 c = texture(tex, vsoTexCoord);
+  fragColor = vec4(c.rgb, 1.0);
+}
