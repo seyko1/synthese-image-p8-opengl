bf351c463060269ee5a9e7288ef560f1cc27f392
François Godin <francois.godin02@etud.univ-paris8.fr>
2025-05-16 06:53:42 +0200

    mandelbulb colors

--- demo_rgb-1.5/shaders/mandelbulbFractal.fs
+++ demo_rgb-1.5/shaders/mandelbulbFractal.fs
@@ -9,8 +9,12 @@ vec3 hsb2rgb(in vec3 c) {
     return c.z + c.y * (rgb-0.5) * (1.0 - abs(2.0 * c.z - 1.0));
 }
 
+float map(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
 void main() {
-    float brightness = vDepth / 255;
+    float brightness = map(vDepth, 0, 20, 0., 1.);
     vec3 hsb = vec3(brightness, 1.0, 0.5); 
 
     fragColor = vec4(hsb2rgb(hsb), 1.0);
